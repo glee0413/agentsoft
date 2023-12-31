b782789c4cfd6d347196f42658d9173a66b997ab
gaoliang <liang.gao@reachauto.com>
2023-12-31 06:27:59 +0000

    添加record的记录功能

--- src/modules/utils.py
+++ src/modules/utils.py
@@ -1,4 +1,9 @@
 import json
+from functools import wraps
+from loguru import logger
+import pickle
+import json
+import os
 
 def custom_value_serializer(value):
     if isinstance(value, dict):
@@ -7,3 +12,35 @@ def custom_value_serializer(value):
         return value.encode()
     else:
         raise TypeError(f"Unsupported value type: {type(value)}")
+
+def record_args(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        params = {'args': args, 'kwargs': kwargs}
+        logger.info({func.__name__,json.dumps(params)})
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@record_args
+@logger.catch
+def my_function(a, b):
+    return a / b
+
+def main():
+    my_function(1, 2)
+    my_function(3, 0)
+    
+    # logger.info("Dumping recorded args to file")
+    # logger.debug(logger._core.handlers[0])
+
+    # with open('args_dump.pkl', 'wb') as file:
+    #     pickle.dump(logger._core.handlers[0].sink.records, file)
+
+    return
+
+if __name__ == "__main__":
+    log_file = os.path.basename(__file__) + '.log'
+    print(log_file)
+    logger.add(log_file,backtrace=True, diagnose=True,rotation="500 MB",serialize=True)
+    main()
