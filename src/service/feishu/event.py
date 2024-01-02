a67200159761b6f069581522900a2de057a94b1e
gaoliang <liang.gao@reachauto.com>
2024-01-02 13:58:16 +0000

    修改正确的json格式

--- src/service/feishu/event.py
+++ src/service/feishu/event.py
@@ -133,8 +133,11 @@ class EventHandler():
             #     message_api_client.send_text_with_open_id(event_box.event.sender.sender_id.open_id, 
             #                                         event_box.event.message.content)
             # )
-            logger.info(f'message:{reply_content} ,event {event_box.event.message.content}')
             feishu_content = json.dumps({'text':f'{reply_content}'})
+            # logger.info(f'message:{reply_content} ,event {event_box.event.message.content}, 
+            #             feishu content {feishu_content} ')
+            logger.info(f'feishu content:{feishu_content}')
+            
             await loop.run_in_executor(
                     None,message_api_client.send_text_with_open_id,
                     event_box.event.sender.sender_id.open_id, 
@@ -178,52 +181,41 @@ class EventHandler():
         
         return
     
-test_json="""
+test_chat_json="""
 {
-    "schema": "2.0",
-    "header": {
-        "event_id": "5e3702a84e847582be8db7fb73283c02",
-        "event_type": "im.message.receive_v1",
-        "create_time": "1608725989000",
-        "token": "rvaYgkND1GOiu5MM0E1rncYC6PLtF7JV",
-        "app_id": "cli_9f5343c580712544",
-        "tenant_key": "2ca1d211f64f6438"
+  "schema": "2.0",
+  "header": {
+    "event_id": "0e6fc6b9c4c9047f9d73847732e754d2",
+    "event_type": "im.message.receive_v1",
+    "create_time": "1704099384553",
+    "token": "LUwSr560MJogoqZzoQxbIeMmEkqB44kq",
+    "app_id": "cli_a5fd980539ba900e",
+    "tenant_key": "1612ab78628e5740"
+  },
+  "event": {
+    "sender": {
+      "sender_id": {
+        "union_id": "on_a6c9c3010bebb2de2d9080d2387b942d",
+        "user_id": "bdbge882",
+        "open_id": "ou_75f5f69117065340f0f7a330f4b054fa"
+      },
+      "sender_type": "user",
+      "tenant_key": "1612ab78628e5740"
     },
-    "event": {
-        "sender": {
-            "sender_id": {
-                "union_id": "on_8ed6aa67826108097d9ee143816345",
-                "user_id": "e33ggbyz",
-                "open_id": "ou_84aad35d084aa403a838cf73ee18467"
-            },
-            "sender_type": "user",
-            "tenant_key": "736588c9260f175e"
-        },
-        "message": {
-            "message_id": "om_5ce6d572455d361153b7cb51da133945",
-            "root_id": "om_5ce6d572455d361153b7cb5xxfsdfsdfdsf",
-            "parent_id": "om_5ce6d572455d361153b7cb5xxfsdfsdfdsf",
-            "create_time": "1609073151345",
-            "update_time": "1687343654666",
-            "chat_id": "oc_5ce6d572455d361153b7xx51da133945",
-            "chat_type": "group",
-            "message_type": "text",
-            "content": "{\"text\":\"@_user_1 hello\"}",
-            "mentions": [
-                {
-                    "key": "@_user_1",
-                    "id": {
-                        "union_id": "on_8ed6aa67826108097d9ee143816345",
-                        "user_id": "e33ggbyz",
-                        "open_id": "ou_84aad35d084aa403a838cf73ee18467"
-                    },
-                    "name": "Tom",
-                    "tenant_key": "736588c9260f175e"
-                }
-            ],
-            "user_agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 13_2_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.53 Safari/537.36 Lark/6.7.5 LarkLocale/en_US ttnet SDK-Version/6.7.8"
-        }
+    "message": {
+      "message_id": "om_5404dc52a18c7e0e7666025e6ca90ab5",
+      "parent_id": null,
+      "root_id": null,
+      "create_time": "1704099384316",
+      "update_time": "1704099384316",
+      "chat_id": "oc_ba1e4e5ae2341aa5656f353df8c29b84",
+      "chat_type": "p2p",
+      "message_type": "text",
+      "content": "{\\"text\\":\\"请写鸡兔同笼的问题python代码\\"}",
+      "mentions": null,
+      "user_agent": null
     }
+  }
 }
 """
 
